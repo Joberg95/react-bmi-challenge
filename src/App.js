9619b421712415c55f59b7d8b1336a0530efb81f
Per Eriksson <per_hegg@hotmail.com>
2019-03-04 18:28:56 +0100

    add button for changing methods

--- src/App.js
+++ src/App.js
@@ -6,22 +6,36 @@ class App extends Component {
     super(props)
     this.state = {
       weight: '',
-      height: ''
+      height: '',
+      methodName: 'metric'
     }
+    this.changeToMetric = this.changeToMetric.bind(this);
+    this.changeToImperial = this.changeToImperial.bind(this);
+
+  }
+
+  changeToMetric() {
+    this.setState({ methodName: 'metric' });
+  }
+
+  changeToImperial() {
+    this.setState({ methodName: 'imperial' });
   }
   
   render() {
     return (
-      <div className="main-div">
+      <div className="bmi-converter">
         <div className="div">
-          <label>Weight(kg)</label>
+          <label id="per">Weight {this.state.methodName === 'metric' ? "(kg)" : "(pounds)"}</label>
           <input name="weight" value={this.state.weight} onChange={e => this.setState({ weight: e.target.value })} />
         </div>
 
         <div className="div">
-          <label>Height(cm)</label>
+          <label id="per">Height {this.state.methodName === 'metric' ? "(cm)" : "(inches)"}</label>
           <input name="height" value={this.state.height} onChange={e => this.setState({ height: e.target.value })} />
         </div>
+        <button id="metricbutton" onClick={this.changeToMetric}> Metric </button>
+        <button id="imperialbutton" onClick={this.changeToImperial}> Imperial</button>
         <DisplayResult
           weight={this.state.weight}
           height={this.state.height}
---
+++ src/__features__/userCanCalculateTheirBMI.feature.js
@@ -0,0 +1,45 @@
+describe('BMI Converter', () => {
+    beforeAll(async () => {
+        await page.goto('http://localhost:3000');
+    });
+
+    beforeEach(async () => {
+        await page.reload();
+    });
+
+    it('should display "BMI Converter" text on page', async () => {
+        await expect(page).toMatch('BMI Converter');
+    });
+
+    describe('Metric method', async () => {
+        beforeEach( async () => {
+            await page.click('button[id="metricbutton"]')
+            await page.type('input[name="weight"]', '95')
+            await page.type('input[name="height"]', '186')
+        })
+
+        it('displays assesment', async () => {   
+            await expect(page).toMatch('You are Overweight')
+        })
+
+        it('displays BMI value', async () => {   
+            await expect(page).toMatch('BMI of 27.46')
+        })
+    })
+
+    describe('Imperial method', async () => {
+        beforeEach( async () => {
+            await page.click('button[id="imperialbutton"]')
+            await page.type('input[name="weight"]', '200')
+            await page.type('input[name="height"]', '73')
+        })
+
+        it('displays assesment', async () => {   
+            await expect(page).toMatch('You are Overweight')
+        })
+
+        it('displays BMI value', async () => {   
+            await expect(page).toMatch('BMI of 26.38')
+        })
+    })
+}); 
