d912d76b9e0e1a187f6d41dfac3f8cbbc076d293
nevroje <joakim.berglind@gmail.com>
2019-03-04 12:56:34 +0100

    fixed undefined error in app.js and added component folder add displayresult js code

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react'
 
 class App extends Component {
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {
       weight: '',
       height: ''
@@ -14,16 +14,16 @@ class App extends Component {
       <div>
         <div>
           <label>Weight(kg)</label>
-          <input name="weight" value={this.state.weight} onChange={this.setState({ weight: e.target.value })} />
+          <input name="weight" value={this.state.weight} onChange={e => this.setState({ weight: e.target.value })} />
         </div>
 
         <div>
           <label>Height(cm)</label>
-          <input name="height" value={this.state.height} onChange={this.setState({ height: e.target.value })} />
+          <input name="height" value={this.state.height} onChange={e => this.setState({ height: e.target.value })} />
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
