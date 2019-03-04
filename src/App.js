c683bbf6b187e917bf7e12c9528ba11cfb00500c
Per Eriksson <per_hegg@hotmail.com>
2019-03-04 11:55:09 +0100

    create some data input for weight and height

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      weight: '',
+      height: ''
+    }
+  }
+  
   render() {
     return (
       <div>
+        <div>
+          <label>Weight(kg)</label>
+          <input name="weight" value={this.state.weight} onChange={this.setState({ weight: e.target.value })} />
+        </div>
+
+        <div>
+          <label>Height(cm)</label>
+          <input name="height" value={this.state.height} onChange={this.setState({ height: e.target.value })} />
+        </div>
       </div>
     );
   }
