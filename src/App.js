6c68fa4b5a9a6c03dacc36ad6a5a95d469f301a7
Per Eriksson <per_hegg@hotmail.com>
2019-03-05 11:38:21 +0100

    refactor tests, one failing

--- src/App.js
+++ src/App.js
@@ -28,14 +28,14 @@ class App extends Component {
         <Header/>
       </div>
         <div>
-          <label>Weight in {this.state.method === "metric" ? "kg" : "lbs"}</label>
+          <label>Weight{this.state.method === "metric" ? "(kg)" : "(lbs)"}</label>
           <input
             name="weight"
             value={this.state.weight}
             onChange={this.handleChange.bind(this)}/>
         </div>
         <div>
-          <label>Height in {this.state.method === "metric" ? "cm" : "inch"}</label>
+          <label>Height{this.state.method === "metric" ? "(cm)" : "(inches)"}</label>
           <input
             name="height"
             value={this.state.height}
@@ -43,7 +43,7 @@ class App extends Component {
            <p>
             <select onChange={this.handleChange.bind(this)} name="method" id="methodSelect" >
               {methodList.map(method => (
-                <option  value={method}>{method}</option>
+                <option key={method} value={method}>{method}</option>
               ))}
             </select>
           </p>
