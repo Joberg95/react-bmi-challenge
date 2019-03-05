a6157040bb5f39229069866a484abdef1df89c50
nevroje <joakim.berglind@gmail.com>
2019-03-05 10:35:25 +0100

    changed header title , all tests green

--- src/header.js
+++ src/header.js
@@ -4,7 +4,7 @@ class Header extends Component {
     render() {
         return (
             <div>
-                <h1>Bmi Calculator</h1>
+                <h1>BMI Converter</h1>
             </div>
         )
     }
